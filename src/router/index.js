8d928ec2794a7c8829e3738994a679919b2e6ad1
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-03-07 15:33:50 +0100

    Initialized project.

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AddNarvaroView from '../views/AddNarvaroView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AddNarvaroView
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
+})
+
+export default router
---
+++ src/views/AddNarvaroView.vue
@@ -0,0 +1,69 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  data() {
+    return {
+      personNum: null
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1 class="sub-header">Välkommen!</h1>
+    <h2 class="mb--big">Anmäl dig nedan</h2>
+
+    <div class="content__wrapper">
+      <form @submit.prevent="onSubmit()">
+        <div class="input--primary__wrapper">
+          <label for="personnummer-input">Personnummer:</label>
+          <input
+            class="input--primary"
+            type="text"
+            id="personnummer-input"
+            name="personnummer-input"
+            size="12"
+            minlength="12"
+            maxlength="12"
+            required
+            pattern="[0-9]+"
+            title="12 siffror, ÅÅÅÅMMDDXXXX"
+            placeholder="ÅÅÅÅMMDDXXXX"
+            autofocus
+            v-model="personNum"
+          />
+        </div>
+
+        <p>
+          Genom att fortsätta godkänner du att vi hanterar dina personuppgifter enligt GDPR, läs mer
+          <RouterLink to="/gdpr" class="link--primary">här</RouterLink>.
+        </p>
+
+        <input type="submit" value="Anmäl" class="btn--primary" />
+      </form>
+    </div>
+
+    <img class="mt--bigger" src="@/assets/images/MHF-Ungdom-logo.png" alt="MHF Ungdom logga." />
+  </main>
+</template>
+
+<style scoped>
+h2 {
+  font-size: 5rem;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form > p {
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  text-wrap: balance;
+  text-align: center;
+}
+</style>
