6474da302c41610faccc2ec0af3ad4750a82d451
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-03-19 19:23:06 +0100

    Moved shell files to script folder, seperate functions and runScripts, add uploadMemberist, bugfixes.

--- backend/scripts/backupFunctions.sh
+++ backend/scripts/backupFunctions.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 #### INITIATE
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Change directory to the script's directory
+cd "$SCRIPT_DIR" || exit
+
 # Load environment variables from .env file
 # Adjust the path if your .env file is located elsewhere
-source .env
+source ".env"
+
+DROPBOX_UPLOADER="../Dropbox-Uploader/dropbox_uploader.sh"
 
 # Temp destination directory for backups
 backup_dir="tempBackups"
+
 # Create the temp backup directory if it doesn't exist
 mkdir -p "$backup_dir"
 
@@ -18,14 +26,23 @@ uploadDatabaseBackup() {
   output_file="$DB_NAME-$timestamp.sql"
   temp_output_file="$backup_dir/$output_file"
 
+  echo "Start upload database backup: $output_file"
+
   # Backup the MySQL database
-  mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" >"$backup_dir/$output_file"
+  mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" >"$temp_output_file"
 
   # Upload temp file to dropbox
-  ./Dropbox-Uploader/dropbox_uploader.sh upload "$temp_output_file" database/"$output_file"
+  $DROPBOX_UPLOADER upload "$temp_output_file" database/"$output_file"
+
+  # Remove temp file
+  rm $temp_output_file
+
+  echo "Finished uploading database backup."
 }
 
 uploadFormattedNarvaro() {
+  echo "Start upload formatted narvaro of current year."
+
   current_year=$(date +'%Y')
   narvaro_table="narvaro_$current_year"
 
@@ -56,14 +73,20 @@ uploadFormattedNarvaro() {
     mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" | tr '\t' ',' | tr '_' '-' >"$temp_output_file"
 
   # Upload temp file to dropbox
-  ./Dropbox-Uploader/dropbox_uploader.sh upload "$temp_output_file" narvaro/"$output_file"
+  $DROPBOX_UPLOADER upload "$temp_output_file" narvaro/"$output_file"
+
+  # Remove temp file
+  rm $temp_output_file
+
+  echo "Finished uploading formatted narvaro of current year."
 }
 
-uploadFormattedMemberlistBackup() {
-  # Timestamp for the backup file
-  timestamp=$(date +%Y-%m-%d)
-  output_file="formattedMedlemslistaBackup_$timestamp.csv"
+uploadFormattedMemberlist() {
+  output_file=$1
+  output_folder=$2
   temp_output_file="$backup_dir/$output_file"
+
+  echo "Start upload formatted memberlist: $output_file"
 
   # MySQL query to select all rows from the member table
   SQL_QUERY="SELECT personnummer, first_name AS förnamn, last_name AS efternamn, end_date AS 'medlem t.o.m' FROM member;"
@@ -72,15 +95,34 @@ uploadFormattedMemberlistBackup() {
   mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -e "$SQL_QUERY" | tr '\t' ',' >"$temp_output_file"
 
   # Upload temp file to dropbox
-  ./Dropbox-Uploader/dropbox_uploader.sh upload "$temp_output_file" medlemslista/"$output_file"
+  $DROPBOX_UPLOADER upload "$temp_output_file" medlemslista/"$output_folder""$output_file"
+
+  # Remove temp file
+  rm $temp_output_file
+
+  echo "Finished uploading formatted memberlist."
+}
+uploadFormattedMemberlistBackup() {
+  # Timestamp for the backup file
+  timestamp=$(date +%Y-%m-%d)
+  output_file="medlemslistaBackup_$timestamp.csv"
+
+  uploadFormattedMemberlist "$output_file" backups/
 }
 
 updateMemberlistFromDropbox() {
+  echo "Start download memberlist and update database memberlist."
+
   dropbox_input_file="medlemslista.csv"
   temp_input_file="$backup_dir/temp_memberlist.csv"
 
   # Download file from Dropbox
-  ./Dropbox-Uploader/dropbox_uploader.sh download medlemslista/"$dropbox_input_file" "$temp_input_file"
+  $DROPBOX_UPLOADER download medlemslista/"$dropbox_input_file" "$temp_input_file"
+  # Check if any errors occurred during the download
+  if [ $? -ne 0 ]; then
+    echo "Error! Stopped downloading memberlist and updating database memberlist."
+    return 1
+  fi
 
   # MySQL query to update the member table
   SQL_UPDATE="CREATE TEMPORARY TABLE temp_member LIKE member;
@@ -105,14 +147,9 @@ updateMemberlistFromDropbox() {
 
   # Execute MySQL query to update member table
   mysql --local_infile=1 -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -e "$SQL_UPDATE"
+
+  # Remove temp file
+  rm $temp_input_file
+
+  echo "Finished downloading memberlist and updating database memberlist."
 }
-
-#### RUN FUNCTIONS
-uploadDatabaseBackup
-uploadFormattedNarvaro
-uploadFormattedMemberlistBackup
-updateMemberlistFromDropbox
-
-#### CLEAN UP
-# Remove tempBackups and its content
-rm -r $backup_dir
---
+++ backend/scripts/uploadMemberlist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#### INITIALIZE
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+# Change directory to the script's directory
+cd "$SCRIPT_DIR" || exit
+
+#### IMPORT
+source ./backupFunctions.sh
+
+#### RUN FUNCTIONS
+uploadFormattedMemberlist medlemslista.csv
